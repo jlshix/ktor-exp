b61ebaac2a1b6f1d8e90dd9e30a9111edbe38268
jlshix <jlshix@163.com>
2022-01-27 17:20:55 +0800

    feat: 首次提交, 生成 ktor 项目

---
+++ src/main/kotlin/com/jlshix/kt/Application.kt
@@ -0,0 +1,14 @@
+package com.jlshix.kt
+
+import io.ktor.application.*
+import com.jlshix.kt.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureHTTP()
+    configureSerialization()
+}
